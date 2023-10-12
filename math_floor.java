15d9e37f5535e9525886c126f12088e826a1539e
vasanthakumar <109866822+Vas2898@users.noreply.github.com>
2023-10-12 16:02:01 +0530

    math_floor

---
+++ math_floor.java
@@ -0,0 +1,13 @@
+public class Mathdemo {
+
+	public static void main(String[] args) {
+		Mathdemo demo = new Mathdemo();
+		demo.floor(64.9);
+
+	}
+
+	private void floor(double no) {
+		int val = (int)no;
+		System.out.println(val);	
+	}
+}
